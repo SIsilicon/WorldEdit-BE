570de6d734685db955de66090bd5077559037660
Roujel Williams <sisilicon28@gmail.com>
2022-01-14 13:50:11 -0500

    Rework localization workflow (#41)
    * Move to .po files for localization.
    
    * delete generated lang files
    
    * Keep text folders
    
    * improved po2lang script

---
+++ po2lang.py
@@ -0,0 +1,128 @@
+import json, glob, re, polib
+import os
+from os.path import relpath
+from itertools import chain
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Converts .po translatation files to .lang Minecraft translatation files.')
+parser.add_argument('--watch', '-w', action='store_true', help='Whether to watch for file changes in the texts folder.')
+args = parser.parse_args()
+
+BPdir = './BP/texts'
+RPdir = './RP/texts'
+srcdir = './texts'
+
+def get_lang(file):
+    return os.path.basename(file).replace('.po', '')
+
+def convert_file(in_path, out_path):
+    newlines = []
+    for entry in polib.pofile(in_path):
+        if not(entry.msgid != 'pack.description' and 'BP' in out_path):
+            string = entry.msgstr.replace('\\"', '"')
+            newlines.append(f'{entry.msgid}={string}\n')
+    
+    with open(out_path, 'w') as file:
+        file.writelines(newlines)
+        print(f'{in_path} converted to {out_path}')
+
+def update_keys(filename):
+    baseKeys = []
+    for entry in polib.pofile(f'{srcdir}/base.pot'):
+        baseKeys.append(entry.msgid)
+    langEntries = {}
+    for entry in polib.pofile(filename):
+        langEntries[entry.msgid] = entry.msgstr.replace('"', '\\"')
+    
+    lines = []
+    with open(f'{srcdir}/base.pot') as file:
+        lines = file.readlines()
+    
+    with open(filename, 'w') as file:
+        msgid = ''
+        for line in lines:
+            if msgid:
+                file.write(f"msgstr \"{langEntries.get(msgid, '')}\"\n")
+                msgid = ''
+            else:
+                match = re.match(r'msgid(.+)"(.+)"', line)
+                if match:
+                    msgid = match.group(2)
+                file.write(line)
+
+def convert_lang(filename):
+    lang = get_lang(filename)
+    convert_file(filename, f'{BPdir}/{lang}.lang')
+    convert_file(filename, f'{RPdir}/{lang}.lang')
+
+def update_lang_json():
+    languages = []
+    for file in glob.iglob(srcdir + '/*.po', recursive = True):
+        languages.append(get_lang(file))
+    
+    for folder in [RPdir, BPdir]:
+        with open(folder + '/languages.json', 'w') as file:
+            file.write('[\n');
+            for i in range(len(languages)):
+                comma = ','
+                if i == len(languages) - 1:
+                    comma = ''
+                file.write(f'    "{languages[i]}"{comma}\n')
+            file.write(']')
+
+for file in chain(glob.iglob(BPdir + '/*.lang'), glob.iglob(RPdir + '/*.lang')):
+    if not 'AUTO_GENERATED' in file:
+        os.remove(file)
+
+for file in glob.iglob(srcdir + '/*.po'):
+    convert_lang(file)
+update_lang_json()
+
+if args.watch:
+    import time
+    from watchdog.observers import Observer
+    from watchdog.events import FileSystemEventHandler
+    
+    def alert_watching():
+        print('Watching for file changes...')
+    
+    class MyHandler(FileSystemEventHandler):
+        def on_modified(self, event):
+            if event.src_path.endswith('.po'):
+                convert_lang(event.src_path)
+                alert_watching()
+            elif event.src_path.endswith('.pot'):
+                for filename in glob.iglob(srcdir + '/*.po', recursive = True):
+                    update_keys(filename)
+                    convert_lang(filename)
+                alert_watching()
+        
+        def on_created(self, event):
+            if event.src_path.endswith('.po'):
+                convert_lang(event.src_path)
+                update_lang_json()
+                alert_watching()
+        
+        def on_deleted(self, event):
+            lang = get_lang(event.src_path)
+            if os.path.exists(BPdir + '/' + lang + '.lang'):
+                os.remove(BPdir + '/' + lang + '.lang')
+            if os.path.exists(RPdir + '/' + lang + '.lang'):
+                os.remove(RPdir + '/' + lang + '.lang')
+            print(f'Deleted {lang}.lang')
+            update_lang_json()
+            alert_watching()
+    
+    observer = Observer()
+    observer.schedule(MyHandler(),  path=srcdir,  recursive=True)
+    observer.start()
+    
+    try:
+        alert_watching()
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    print('\n')
+    observer.join()
