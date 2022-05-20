215d405c64f80307b38bb0cb4a0fa61652938d3b
SIsilicon <sisilicon28@gmail.com>
2022-05-20 15:58:10 -0500

    Incremented addon version

--- src/config.ts
+++ src/config.ts
@@ -54,4 +54,4 @@ export const ASYNC_TIME_BUDGET: number = 200;
 export const FAST_MODE: boolean = false;
 
 // The version of WorldEdit (do not change)
-export const VERSION: string = '0.6.1 [beta]';
+export const VERSION: string = '0.6.3 [beta]';
