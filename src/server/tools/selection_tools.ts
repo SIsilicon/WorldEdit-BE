1f7fb9986064079c8d5c01ecbb61edd604145849
SIsilicon <sisilicon28@gmail.com>
2022-06-04 15:40:17 -0500

    Moved selection tool messages back to action bar

--- src/server/tools/selection_tools.ts
+++ src/server/tools/selection_tools.ts
@@ -7,9 +7,11 @@ import { Server } from '@notbeer-api';
 class SelectionTool extends Tool {
     permission = 'worldedit.selection.pos';
     useOn = function (self: Tool, player: Player, session: PlayerSession, loc: BlockLocation) {
+        session.usingItem = true;
         Server.command.callCommand(player, player.isSneaking ? 'pos1' : 'pos2',
             [`${loc.x}`, `${loc.y}`, `${loc.z}`]
         );
+        session.usingItem = false;
     }
 }
 Tools.register(SelectionTool, 'selection_wand');
@@ -17,7 +19,9 @@ Tools.register(SelectionTool, 'selection_wand');
 class FarSelectionTool extends Tool {
     permission = 'worldedit.selection.hpos';
     use = function (self: Tool, player: Player, session: PlayerSession) {
+        session.usingItem = true;
         Server.command.callCommand(player, player.isSneaking ? 'hpos1' : 'hpos2');
+        session.usingItem = false;
     }
 }
 Tools.register(FarSelectionTool, 'far_selection_wand');
