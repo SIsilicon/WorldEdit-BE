689dfb10066b1f6a2da06cfacfc357873445933b
SIsilicon <sisilicon28@gmail.com>
2022-06-10 10:11:25 -0500

    Made manifest files built from one file

--- build.py
+++ build.py
@@ -3,11 +3,13 @@ import subprocess, sys, os, shutil
 import argparse, re
 
 parser = argparse.ArgumentParser(description='Build and package the addon.')
-parser.add_argument('--watch', '-w', action='store_true', help='Whether to continually build the project while editing it.')
-parser.add_argument('--target', choices=['release', 'debug'], default='debug', help='Whether to build the addon in debug or release mode.')
+parser.add_argument('--watch', '-w', choices=['release', 'preview', 'server'], help='Whether to continually build and where to sync the project while editing it.')
+parser.add_argument('--target', choices=['release', 'debug', 'release_server'], default='debug', help='Whether to build the addon in debug or release mode.')
 parser.add_argument('--clean', '-c', action='store_true', help='Clean "BP/scripts" folder before building.')
 parser.add_argument('--package-only', '-p', action='store_true', help='Only package what\'s already there.')
 args = parser.parse_args()
+
+build_pack_name = 'WorldEdit'
 
 def regExpSub(regEx, replace, file):
     with open(file, 'r') as f:
@@ -42,6 +44,7 @@ if not args.package_only:
     if args.watch:
         print('syncing com.mojang folder...')
         subprocess.call([sys.executable, 'tools/sync2com-mojang.py'], stdout=subprocess.DEVNULL)
+        subprocess.call([sys.executable, 'tools/process_manifest.py'], stdout=subprocess.DEVNULL)
 
         print('Watch mode: press control-C to stop.')
         tsc = subprocess.Popen('tsc -w', shell=True)
@@ -50,7 +53,7 @@ if not args.package_only:
         # Convert po to lang files
         po2lang = subprocess.Popen([sys.executable, 'tools/po2lang.py', '-w'], stdout=subprocess.DEVNULL)
         # Sync to com.mojang
-        sync_mojang = subprocess.Popen([sys.executable, 'tools/sync2com-mojang.py', '-w', '--init=False'], stdout=subprocess.DEVNULL)
+        sync_mojang = subprocess.Popen([sys.executable, 'tools/sync2com-mojang.py', '-w', '--init=False', f'--dest={args.watch}'], stdout=subprocess.DEVNULL)
         
         from time import sleep
         try:
@@ -73,34 +76,31 @@ if not args.package_only:
     subprocess.call([sys.executable, 'tools/remap_imports.py'])
     # Convert po to lang files
     subprocess.call([sys.executable, 'tools/po2lang.py'])
+    # Build manifests
+    subprocess.call([sys.executable, 'tools/process_manifest.py', f'--target={args.target}'], stdout=subprocess.DEVNULL)
 
 if not os.path.isdir('builds'):
     os.makedirs('builds')
 
-if os.path.exists('builds/WeditBP'):
-    shutil.rmtree('builds/WeditBP')
-if os.path.exists('builds/WeditRP'):
-    shutil.rmtree('builds/WeditRP')
-try: shutil.copytree('BP', 'builds/WeditBP')
+if os.path.exists(f'builds/{build_pack_name}BP'):
+    shutil.rmtree(f'builds/{build_pack_name}BP')
+if os.path.exists(f'builds/{build_pack_name}RP'):
+    shutil.rmtree(f'builds/{build_pack_name}RP')
+try: shutil.copytree('BP', f'builds/{build_pack_name}BP')
 except: pass
-try: shutil.copytree('RP', 'builds/WeditRP')
+try: shutil.copytree('RP', f'builds/{build_pack_name}RP')
 except: pass
 
 if args.target == 'release':
     from zipfile import ZipFile;
-
+    
     def zipWriteDir(zip, dirname, arcname):
         for folderName, _, filenames in os.walk(dirname):
             for filename in filenames:
                 filePath = os.path.join(folderName, filename)
                 zip.write(filePath, arcname / Path(filePath).relative_to(dirname))
     
-    with ZipFile('builds/WorldEdit.mcaddon', 'w') as zip:
-        zipWriteDir(zip, 'builds/WeditBP', 'WorldEditBP')
-        zipWriteDir(zip, 'builds/WeditRP', 'WorldEditRP')
-    
-else:
-    
-    regExpSub('"name":(.+)",', r'"name":\1(Dev)",', 'builds/WeditBP/manifest.json')
-    regExpSub('"name":(.+)",', r'"name":\1(Dev)",', 'builds/WeditRP/manifest.json')
+    with ZipFile(f'builds/{build_pack_name}.mcaddon', 'w') as zip:
+        zipWriteDir(zip, f'builds/{build_pack_name}BP', f'{build_pack_name}BP')
+        zipWriteDir(zip, f'builds/{build_pack_name}RP', f'{build_pack_name}RP')
     
