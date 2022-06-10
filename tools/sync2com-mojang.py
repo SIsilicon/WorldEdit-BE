0543930d9997ea19b19a711ae7e48612fb314f1a
SIsilicon <sisilicon28@gmail.com>
2022-06-10 10:10:25 -0500

    Made manifest files built from one file, Added build mode for servers

--- tools/sync2com-mojang.py
+++ tools/sync2com-mojang.py
@@ -2,21 +2,23 @@ from pathlib import Path
 import glob, os, shutil
 import argparse
 
-parser = argparse.ArgumentParser(description='Syncs the project folder\'s data with com.mojang (Windows 10/11 only).\nNote: Will only sync CHANGED files in watch mode.')
+parser = argparse.ArgumentParser(description='Syncs the project folder\'s data with Minecraft (Windows 10/11 only).\nNote: Will only sync CHANGED files in watch mode.')
 parser.add_argument('--watch', '-w', action='store_true', help='Whether to watch for file changes.')
 parser.add_argument('--init', choices=['False', 'True'], default='True', help='Whether to initially sync com.mojang before watching file changes.')
+parser.add_argument('--dest', choices=['release', 'preview', 'server'], default='release', help='The place to sync the addon to')
 args = parser.parse_args()
 
-com_mojang = os.path.expandvars('%localappdata%\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang')
-dev_bev_pack = com_mojang + '\\development_behavior_packs\\WorldEdit BP'
-dev_res_pack = com_mojang + '\\development_resource_packs\\WorldEdit RP'
+if args.dest == 'release':
+    com_mojang = os.path.expandvars('%localappdata%\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang')
+elif args.dest == 'preview':
+    com_mojang = os.path.expandvars('%localappdata%\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe\\LocalState\\games\\com.mojang')
+elif args.dest == 'server':
+    com_mojang = os.path.expandvars('C:\\Minecraft Servers\\1.19.1')
 
-sync_preview = False
-if os.path.exists(os.path.expandvars('%localappdata%\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe')):
-    sync_preview = True
-    PREV_com_mojang = os.path.expandvars('%localappdata%\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe\\LocalState\\games\\com.mojang')
-    PREV_dev_bev_pack = PREV_com_mojang + '\\development_behavior_packs\\WorldEdit BP'
-    PREV_dev_res_pack = PREV_com_mojang + '\\development_resource_packs\\WorldEdit RP'
+pack_folder = 'WorldEdit'
+
+behaviour_pack = com_mojang + f'\\development_behavior_packs\\{pack_folder} BP'
+resource_pack = com_mojang + f'\\development_resource_packs\\{pack_folder} RP'
 
 def sync_file(path, from_root, to_root):
     from_file = Path(path).relative_to(from_root)
@@ -39,22 +41,15 @@ def remove_dir_if_exists(path):
         shutil.rmtree(path)
 
 def sync_all():
-    remove_dir_if_exists(dev_bev_pack)
-    remove_dir_if_exists(dev_res_pack)
-    if sync_preview:
-        remove_dir_if_exists(PREV_dev_bev_pack)
-        remove_dir_if_exists(PREV_dev_res_pack)
+    remove_dir_if_exists(behaviour_pack)
+    remove_dir_if_exists(resource_pack)
 
     for file in glob.iglob('BP/**', recursive=True):
         if os.path.isfile(file):
-            sync_file(file, './BP', dev_bev_pack)
-            if sync_preview:
-                sync_file(file, './BP', PREV_dev_bev_pack)
+            sync_file(file, './BP', behaviour_pack)
     for file in glob.iglob('RP/**', recursive=True):
         if os.path.isfile(file):
-            sync_file(file, './RP', dev_res_pack)
-            if sync_preview:
-                sync_file(file, './RP', PREV_dev_res_pack)
+            sync_file(file, './RP', resource_pack)
 
 if args.watch:
     import time
@@ -70,13 +65,9 @@ if args.watch:
         
         def update(self, path):
             if self.packtype == 'BP':
-                sync_file(path, './BP', dev_bev_pack)
-                if sync_preview:
-                    sync_file(path, './BP', PREV_dev_bev_pack)
+                sync_file(path, './BP', behaviour_pack)
             else:
-                sync_file(path, './RP', dev_res_pack)
-                if sync_preview:
-                    sync_file(path, './RP', PREV_dev_res_pack)
+                sync_file(path, './RP', resource_pack)
         
         def on_modified(self, ev):
             if not ev.is_directory:
