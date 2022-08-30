9088179a164b1bb61e544744ab02cc6e58563c6c
Fynn Gutic <66220444+Fynn93@users.noreply.github.com>
2022-08-30 12:33:50 +0200

    Fix

--- src/server/commands/navigation/ascend.ts
+++ src/server/commands/navigation/ascend.ts
@@ -1,15 +1,22 @@
 import { PlayerUtil } from "@modules/player_util.js";
 import { RawText } from "@notbeer-api";
 import { registerCommand } from "../register_commands.js";
-import { BlockLocation, Location } from "mojang-minecraft";
+import { BlockLocation, Location, Player } from "mojang-minecraft";
 
 const registerInformation = {
     name: "ascend",
     permission: "worldedit.navigation.ascend",
-    description: "commands.wedit:ascend.description"
+    description: "commands.wedit:ascend.description",
+    usage: [
+        {
+            name: "levels",
+            type: "int",
+            default: 1
+        }
+    ]
 };
 
-registerCommand(registerInformation, function (session, builder, args) {
+function ascend(builder: Player) {
     let location = PlayerUtil.getBlockLocation(builder);
     const dimension = builder.dimension;
 
@@ -25,10 +32,20 @@ registerCommand(registerInformation, function (session, builder, args) {
         if (!dimension.getBlock(head).isEmpty) invalid = true
 
         if (!invalid) {
+
             builder.teleport(new Location(location.x, legs.y, location.z), dimension, 0, 0);
-            return RawText.translate("commands.wedit:ascend.explain");
+            return RawText.translate("commands.wedit:thru.explain");
         }
     }
+    return RawText.translate("commands.wedit:ascend.obstructed")
+}
 
-    return RawText.translate("commands.wedit:ascend.obstructed");
+registerCommand(registerInformation, function (session, builder, args) {
+    let levels = args.get("levels") as number;
+
+    for (let level = 0; level < levels; level++) {
+        ascend(builder);
+    }
+
+    return RawText.translate("commands.wedit:thru.explain");
 });
--- src/server/commands/navigation/descend.ts
+++ src/server/commands/navigation/descend.ts
@@ -1,15 +1,22 @@
 import { PlayerUtil } from "@modules/player_util.js";
 import { RawText } from "@notbeer-api";
 import { registerCommand } from "../register_commands.js";
-import { BlockLocation, Location } from "mojang-minecraft";
+import { BlockLocation, Location, Player } from "mojang-minecraft";
 
 const registerInformation = {
     name: "descend",
     permission: "worldedit.navigation.descend",
-    description: "commands.wedit:descend.description"
+    description: "commands.wedit:descend.description",
+    usage: [
+        {
+            name: "levels",
+            type: "int",
+            default: 1
+        }
+    ]
 };
 
-registerCommand(registerInformation, function (session, builder, args) {
+function descend(builder: Player) {
     let location = PlayerUtil.getBlockLocation(builder);
     const dimension = builder.dimension;
 
@@ -25,10 +32,20 @@ registerCommand(registerInformation, function (session, builder, args) {
         if (!dimension.getBlock(head).isEmpty) invalid = true
 
         if (!invalid) {
+
             builder.teleport(new Location(location.x, legs.y, location.z), dimension, 0, 0);
-            return RawText.translate("commands.wedit:descend.explain");
+            return RawText.translate("commands.wedit:thru.explain");
         }
     }
+    return RawText.translate("commands.wedit:descend.obstructed")
+}
 
-    return RawText.translate("commands.wedit:descend.obstructed");
+registerCommand(registerInformation, function (session, builder, args) {
+    let levels = args.get("levels") as number;
+
+    for (let level = 0; level < levels; level++) {
+        descend(builder);
+    }
+
+    return RawText.translate("commands.wedit:thru.explain");
 });
