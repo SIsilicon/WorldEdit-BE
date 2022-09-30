f8a26162d5e33b15d30cc191f68b491c7642830a
SIsilicon <sisilicon28@gmail.com>
2022-09-30 14:01:31 -0500

    Fixed timeout functions Fixed navigation tool not printing to action bar

--- src/server/tools/navigation_tool.ts
+++ src/server/tools/navigation_tool.ts
@@ -3,11 +3,13 @@ import { Tool } from "./base_tool.js";
 import { Tools } from "./tool_manager.js";
 import { PlayerUtil } from "@modules/player_util.js";
 import { Server } from "@notbeer-api";
+import { PlayerSession } from "server/sessions.js";
 
 class NavigationTool extends Tool {
   permission = "worldedit.navigation";
 
-  use = function (self: Tool, player: Player) {
+  use = function (self: Tool, player: Player, session: PlayerSession) {
+    session.usingItem = true;
     if (player.dimension.getBlock(PlayerUtil.getBlockLocation(player).offset(0, 1, 0)).type.id != "minecraft:air") {
       Server.command.callCommand(player, "unstuck", []);
     } else if (player.isSneaking) {
@@ -15,6 +17,7 @@ class NavigationTool extends Tool {
     } else {
       Server.command.callCommand(player, "jumpto", []);
     }
+    session.usingItem = false;
   };
 }
 Tools.register(NavigationTool, "navigation_wand");
