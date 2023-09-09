d6a837389286207cc016f3e06f6d19784fddbd25
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2023-09-09 17:47:28 +0000

    eslint fix

--- src/server/commands/selection/minsel.ts
+++ src/server/commands/selection/minsel.ts
@@ -26,7 +26,7 @@ registerCommand(registerInformation, function* (session, builder, args) {
   for (const axes of [["x", "y", "z"], ["y", "z", "x"], ["z", "x", "y"]] as ("x"|"y"|"z")[][]) {
     layers: for (const [layer, offset] of [[min, 1], [max, -1]] as [Vector, number][]) {
       do {
-        vec[axes[0]] = layer[axes[0]];      
+        vec[axes[0]] = layer[axes[0]];
         for (vec[axes[1]] = min[axes[1]]; vec[axes[1]] <= max[axes[1]]; vec[axes[1]]++) {
           for (vec[axes[2]] = min[axes[2]]; vec[axes[2]] <= max[axes[2]]; vec[axes[2]]++) {
             if (mask.matchesBlock(dimension.getBlock(vec))) continue layers;
