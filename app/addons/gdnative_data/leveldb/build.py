6061a3a89147f835fb8aa09dd4bf3ee85da526f0
Pablo Alexis Dominguez Grau <pabloadominguezgrau@gmail.com>
2024-02-17 11:27:51 -0500

    Lint python files with black

--- app/addons/gdnative_data/leveldb/build.py
+++ app/addons/gdnative_data/leveldb/build.py
@@ -1,4 +1,3 @@
-
 import os
 from distutils.command.build import build
 from pathlib import Path
@@ -10,15 +9,22 @@ def execute(args):
     target_file_path = args["target_file_path"]
     build_target = "Debug" if args["target"] == "debug" else "Release"
 
-    run_command(str(source_path / "build"), [
-        "cmake", "--build", ".", "--target", "leveldb", "--config", build_target
-    ])
-    
+    run_command(
+        str(source_path / "build"),
+        ["cmake", "--build", ".", "--target", "leveldb", "--config", build_target],
+    )
+
     if os.path.exists(target_file_path + ".exp"):
         os.remove(target_file_path + ".exp")
     if os.path.exists(target_file_path + ".lib"):
         os.remove(target_file_path + ".lib")
 
-    os.rename(source_path / f"build/{build_target}/leveldb.dll", target_file_path + ".dll")
-    os.rename(source_path / f"build/{build_target}/leveldb.exp", target_file_path + ".exp")
-    os.rename(source_path / f"build/{build_target}/leveldb.lib", target_file_path + ".lib")
+    os.rename(
+        source_path / f"build/{build_target}/leveldb.dll", target_file_path + ".dll"
+    )
+    os.rename(
+        source_path / f"build/{build_target}/leveldb.exp", target_file_path + ".exp"
+    )
+    os.rename(
+        source_path / f"build/{build_target}/leveldb.lib", target_file_path + ".lib"
+    )
