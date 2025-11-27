7018fd5934391504731a77cf5f773838f4be3f2b
Roujel Williams <sisilicon28@gmail.com>
2025-11-27 08:52:03 -0500

    Added optional mask argument to ;faces and ;walls

--- src/server/commands/region/faces.ts
+++ src/server/commands/region/faces.ts
@@ -2,12 +2,16 @@ import { assertSelection } from "@modules/assert.js";
 import { Jobs } from "@modules/jobs.js";
 import { CommandInfo, RawText } from "@notbeer-api";
 import { registerCommand } from "../register_commands.js";
+import { Mask } from "@modules/mask.js";
 
 const registerInformation: CommandInfo = {
     name: "faces",
     permission: "worldedit.region.faces",
     description: "commands.wedit:faces.description",
-    usage: [{ name: "pattern", type: "Pattern" }],
+    usage: [
+        { name: "pattern", type: "Pattern" },
+        { name: "mask", type: "Mask", default: new Mask() },
+    ],
 };
 
 registerCommand(registerInformation, function* (session, builder, args) {
@@ -17,8 +21,9 @@ registerCommand(registerInformation, function* (session, builder, args) {
     }
 
     const pattern = args.get("_using_item") ? session.globalPattern : args.get("pattern");
+    const mask = args.get("_using_item") ? undefined : args.get("mask");
 
     const [shape, loc] = session.selection.getShape();
-    const count = yield* Jobs.run(session, 2, shape.generate(loc, pattern, null, session, { hollow: true }));
+    const count = yield* Jobs.run(session, 2, shape.generate(loc, pattern, mask, session, { hollow: true }));
     return RawText.translate("commands.blocks.wedit:changed").with(`${count}`);
 });
--- src/server/commands/region/walls.ts
+++ src/server/commands/region/walls.ts
@@ -2,12 +2,16 @@ import { assertSelection } from "@modules/assert.js";
 import { Jobs } from "@modules/jobs.js";
 import { CommandInfo, RawText } from "@notbeer-api";
 import { registerCommand } from "../register_commands.js";
+import { Mask } from "@modules/mask.js";
 
 const registerInformation: CommandInfo = {
     name: "walls",
     permission: "worldedit.region.walls",
     description: "commands.wedit:wall.description",
-    usage: [{ name: "pattern", type: "Pattern" }],
+    usage: [
+        { name: "pattern", type: "Pattern" },
+        { name: "mask", type: "Mask", default: new Mask() },
+    ],
 };
 
 registerCommand(registerInformation, function* (session, builder, args) {
@@ -17,8 +21,9 @@ registerCommand(registerInformation, function* (session, builder, args) {
     }
 
     const pattern = args.get("_using_item") ? session.globalPattern : args.get("pattern");
+    const mask = args.get("_using_item") ? undefined : args.get("mask");
 
     const [shape, loc] = session.selection.getShape();
-    const count = yield* Jobs.run(session, 2, shape.generate(loc, pattern, null, session, { wall: true, hollow: true }));
+    const count = yield* Jobs.run(session, 2, shape.generate(loc, pattern, mask, session, { wall: true, hollow: true }));
     return RawText.translate("commands.blocks.wedit:changed").with(`${count}`);
 });
