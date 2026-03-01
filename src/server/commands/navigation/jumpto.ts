d2f56ada501071e5c969e8cf41c9ffc1eb0bace3
Roujel Williams <sisilicon28@gmail.com>
2026-03-01 17:18:10 -0500

    Limit ;jumpto command within loaded chunks

--- src/server/commands/navigation/jumpto.ts
+++ src/server/commands/navigation/jumpto.ts
@@ -1,5 +1,5 @@
 import { PlayerUtil } from "@modules/player_util.js";
-import { CommandInfo, RawText, sleep } from "@notbeer-api";
+import { CommandInfo, RawText, Vector, sleep } from "@notbeer-api";
 import { getCommandFunc, registerCommand } from "../register_commands.js";
 import config from "config.js";
 
@@ -11,8 +11,55 @@ const registerInformation: CommandInfo = {
 };
 
 registerCommand(registerInformation, function* (session, builder) {
-    const hit = PlayerUtil.traceForBlock(builder, config.traceDistance);
-    builder.runCommand(`tp ${hit.x} ${hit.y} ${hit.z}`);
+    let hit = PlayerUtil.traceForBlock(builder);
+    const start = builder.getHeadLocation();
+    const dir = builder.getViewDirection();
+    const maxDistance = config.traceDistance;
+    if (!hit || !builder.dimension.isChunkLoaded(hit)) {
+        let lastLoadedPoint = Vector.from(start).floor();
+        let distance = 0;
+        const epsilon = 1e-4;
+
+        while (distance < maxDistance) {
+            const sampleDistance = Math.min(distance + epsilon, maxDistance);
+            const sample = Vector.add(start, Vector.mul(dir, sampleDistance));
+            const chunkSample = sample.floor();
+
+            if (!builder.dimension.isChunkLoaded(chunkSample)) break;
+
+            const chunkMinX = Math.floor(sample.x / 16) * 16;
+            const chunkMinZ = Math.floor(sample.z / 16) * 16;
+
+            let tExitX = Infinity;
+            if (dir.x > 0) tExitX = (chunkMinX + 16 - start.x) / dir.x;
+            else if (dir.x < 0) tExitX = (chunkMinX - start.x) / dir.x;
+
+            let tExitZ = Infinity;
+            if (dir.z > 0) tExitZ = (chunkMinZ + 16 - start.z) / dir.z;
+            else if (dir.z < 0) tExitZ = (chunkMinZ - start.z) / dir.z;
+
+            const exitDistance = Math.min(tExitX, tExitZ, maxDistance);
+            if (!Number.isFinite(exitDistance)) {
+                const endPoint = new Vector(start.x + dir.x * maxDistance, start.y + dir.y * maxDistance, start.z + dir.z * maxDistance).floor();
+                if (builder.dimension.isChunkLoaded(endPoint)) lastLoadedPoint = endPoint;
+                break;
+            }
+
+            if (exitDistance <= distance + epsilon) {
+                distance += epsilon;
+                continue;
+            }
+
+            const segmentEnd = new Vector(start.x + dir.x * exitDistance, start.y + dir.y * exitDistance, start.z + dir.z * exitDistance).floor();
+            if (!builder.dimension.isChunkLoaded(segmentEnd)) break;
+
+            lastLoadedPoint = segmentEnd;
+            distance = exitDistance;
+        }
+
+        hit = lastLoadedPoint;
+    }
+    builder.teleport(hit);
     while (!builder.dimension.getBlock(hit)) yield sleep(1);
     getCommandFunc("unstuck")(session, builder, new Map());
 
